3914e30da1c460c152059e5f02e4204d214122ad
kelingjie1 <kelingjie1@126.com>
2014-10-22 12:26:47 +0800

    修改资源名字，开发游戏场景

---
+++ src/Assets/Scripts/Model/Game/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PropType
+{
+
+}
+public class GameController : MonoBehaviour 
+{
+    public bool running;
+    public int Speed = Setting.Instance.defaultSetting.GetInt("StartSpeed");
+
+    GameObject Map;
+    GameObject Mountain;
+	void Awake()
+    {
+        Map = gameObject.FindChild("Map");
+        Mountain = gameObject.FindChild("Mountain");
+        
+    }
+	void Update () 
+    {
+        if (running)
+        {
+            Map.transform.localPosition = new Vector3(0, Map.transform.localPosition.y - Time.deltaTime * Speed, 0);
+            int mountainy = (int)(Map.transform.localPosition.y / Screen.height);
+            Mountain.transform.localPosition = new Vector3(0, -mountainy * Screen.height, 0);
+            
+        }
+	}
+}
---
+++ src/Assets/Scripts/Model/Game/IceBlock.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceBlock : MonoBehaviour 
+{
+    public int rowId;
+    public int columnId;
+    public enum IceType
+    {
+
+    }
+    UISprite IceSprite;
+    UISprite PropSprite;
+    IceType m_iceType;
+    public IceType iceType
+    {
+        get
+        {
+            return m_iceType;
+        }
+        set
+        {
+            m_iceType = value;
+        }
+    }
+    PropType m_propType;
+    public PropType propType
+    {
+        get
+        {
+            return m_propType;
+        }
+        set
+        {
+            m_propType = value;
+        }
+    }
+	void Awake () 
+    {
+        IceSprite = gameObject.FindChild("IceSprite").GetComponent<UISprite>();
+        PropSprite = gameObject.FindChild("PropSprite").GetComponent<UISprite>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
